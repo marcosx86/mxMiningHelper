401336b08312b98718fa17c4906a8d615b272809
Marcos Silva Cunha <marcos21xx@gmail.com>
2016-02-10 20:56:03 -0300

    Empty plugin, Eclipse project, Maven configuration

---
+++ src/main/java/net/m21xx/mc/mininghelper/mxMiningHelper.java
@@ -0,0 +1,19 @@
+package net.m21xx.mc.mininghelper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class mxMiningHelper extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+	
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
+
+}
